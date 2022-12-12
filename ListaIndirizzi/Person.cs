01ee64e091cf4437a1f32bc3a95986eaa186f628
RedGuardiaN7 <artiom.str@hotmail.it>
2022-12-12 14:58:15 +0100

    Inizio lettura del file e creazione delle classi

---
+++ ListaIndirizzi/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    internal class Address
+    {
+
+        //Attributi
+
+        public string? Street { get; set; }
+
+        public string? City { get; set; }
+
+        public string? Provnice { get; set; }
+
+        public string? ZIP { get; set; }
+
+    }
+}
--- ListaIndirizzi/Program.cs
+++ ListaIndirizzi/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿//Consegna:  leggere un file CSV e salvare tutti gli indirizzi contenuti al sul interno all’interno di una lista di oggetti istanziati a partire dalla classe Indirizzo
+
+try
+{
+    StreamReader file = File.OpenText("F:/Users/Artiom/Desktop/Esercizi GIT/ListaIndirizzi/ListaIndirizzi/addresses.csv");
+
+    while (!file.EndOfStream)
+    {
+        string line = file.ReadLine();
+        Console.WriteLine(line);
+    }
+
+    file.Close();
+
+}
+
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
